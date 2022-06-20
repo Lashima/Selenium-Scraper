3bfda12e0226e3d289c4f73a8741912cfb0bf1a5
Lashima- <45839126+Lashima@users.noreply.github.com>
2022-06-20 20:22:47 +0000

    generated csv file from extracted data

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+import pandas as pd
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
 
 def get_driver():
   chrome_options = Options()
@@ -10,22 +12,27 @@ def get_driver():
   chrome_options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=chrome_options)
   return driver
+
 def get_videos(driver):
-  YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
   driver.get(YOUTUBE_TRENDING_URL)
-  video_div_class='ytd-video-renderer'
-  videos = driver.find_elements(By.CLASS_NAME, video_div_class)
+  #time.sleep(5)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
   return videos
 
 def parse_video(video):
-  title_tag = video.find_elements(By.ID, 'inline-title-icon')
+  title_tag = video.find_element(By.ID, 'video-title')
   title = title_tag.text
   url = title_tag.get_attribute('href')
-  thumbnail_tag = video.find_elements(By.TAG_NAME, 'img')
+  
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
   thumbnail_url = thumbnail_tag.get_attribute('src')
-  channel_div = video.find_elements(By.CLASS_NAME, 'ytd-channel-name')
+
+  channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
   channel_name = channel_div.text
-  description = video.find_elements(By.ID, 'description-text').text
+  
+  description = video.find_element(By.ID, 'description-text').text
+
   return {
     'title': title,
     'url': url,
@@ -33,13 +40,20 @@ def parse_video(video):
     'channel': channel_name,
     'description': description
   }
+
 if __name__ == "__main__":
   print('Creating driver')
   driver = get_driver()
+
   print('Fetching trending videos')
   videos = get_videos(driver)
   
   print(f'Found {len(videos)} videos')
+
   print('Parsing top 10 videos')
   videos_data = [parse_video(video) for video in videos[:10]]
-  print('videos_data',)
+  print(videos_data)
+  print('Save the data to a CSV')
+  videos_df = pd.DataFrame(videos_data)
+  print(videos_df)
+  videos_df.to_csv('trending.csv', index=None)
